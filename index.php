864d278a7f3b43ebd3e32558fefe293c67a42186
D4riu5 <R3iji94@gmail.com>
2023-03-05 03:37:53 +0100

    V 0.1 - added: Movie class with variables, constructor and a function. - added: 2 movies initiated and printed on page

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP object oriented programming 1</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+</head>
+<body class="bg-dark text-white">
+    
+    
+
+    <?php
+        class Movie {
+            public $title;
+            public $year;
+            public $rating;
+            public $votes;
+
+            function __construct($_title, $_year, $_rating, $_votes) {
+                $this->title = $_title;
+                $this->year = $_year;
+                $this->rating = $_rating;
+                $this->votes = $_votes;
+            }
+
+            function displayInfo() {
+                echo "Title: " . $this->title . "<br>";
+                echo "Year: " . $this->year . "<br>";
+                echo "Rating: " . $this->rating . "<br>";
+                echo "Votes: " . $this->votes . "<br>";
+
+            }
+        }
+
+        $movie1 = new Movie("The Shawshank Redemption", 1994, 9.4, "2.7m");
+        $movie2 = new Movie("Donnie Darko", 2001, 8.0, "811k");
+
+        $movie1->displayInfo();
+        echo "<br>";
+        $movie2->displayInfo();
+
+    ?>
+
+
+
+
+
+
+</body>
+</html>
